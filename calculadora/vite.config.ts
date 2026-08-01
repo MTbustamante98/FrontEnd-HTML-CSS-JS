ce0fc1667c4f9dce7941b1ae0cda07ffb6cbfab0
Matheus Bustamante <mtbustamante98@gmail.com>
2026-08-01 15:42:43 -0300

    update

--- calculadora/vite.config.ts
+++ calculadora/vite.config.ts
@@ -3,5 +3,5 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [tailwindcss()],
-  base: "FrontEnd-HTML-CSS-JS"
+  base: "FrontEnd-HTML-CSS-JS/caculadora/"
 });
